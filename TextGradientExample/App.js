13afaa92db382841580ae96ba05c44420a4d75da
Igor Yegorov <iegoroff@gmail.com>
2018-07-06 22:29:03 +0300

    updated example & gradle config

--- TextGradientExample/App.js
+++ TextGradientExample/App.js
@@ -1,37 +1,32 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ *
+ * @format
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 import {LinearTextGradient} from 'react-native-text-gradient';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component {
   render() {
-
     return (
       <View style={styles.container}>
         <LinearTextGradient
-          ref={ref => console.warn('linear')}
           style={styles.welcome}
-          colors={['red', 'green']}
           locations={[0, 1]}
-          start={{x: 0, y: 0}}
-          end={{x: 1, y: 0}}
+          colors={['red', 'blue']}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
         >
           Welcome to React Native!
         </LinearTextGradient>
@@ -51,10 +46,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
